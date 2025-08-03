6437ebab20d7744de0adbb0c8fcd4f2682fe6fa5
tcareyintx <tlcareyintx@gmail.com>
2025-08-03 13:46:09 +0000

    Fix the number min-max, initial tests for select

--- tests/test_select.py
+++ tests/test_select.py
@@ -1,13 +1,12 @@
-"""Test SkybellGen number."""
+"""Test SkybellGen select."""
 
 import pytest
 
-from aioskybellgen.helpers import const as CONST
 
-from homeassistant.components.number import (
-    DOMAIN as NUMBER_DOMAIN,
-    SERVICE_SET_VALUE,
-    ATTR_VALUE
+from homeassistant.components.select import (
+    DOMAIN as SELECT_DOMAIN,
+    SERVICE_SELECT_OPTION,
+    ATTR_OPTION
 )
 
 from homeassistant.const import Platform, ATTR_ENTITY_ID
@@ -17,18 +16,17 @@ import homeassistant.helpers.entity_registry as er
 
 from .conftest import async_init_integration
 
-NUMBER = Platform.NUMBER
+SELECT = Platform.SELECT
 
 TEST_ENTITIES = [
-    ["number.frontdoor_facial_detection_sensitivity", "53.4", 50.0],
-    ["number.frontdoor_location_latitude", "1.0", 1.0],
-    ["number.frontdoor_location_longitude", "-1.0", 1.0],
-    ["number.frontdoor_human_detection_sensitivity", "50.0", 1],
+    ["select.frontdoor_outdoor_chime_volume", "High", "Low"],
+    ["select.frontdoor_speaker_volume", "Medium", "Low"],
+    ["select.frontdoor_image_quality", "Low", "High"]
 ]
 
 
-async def test_number_service(hass, remove_platforms, bypass_initialize):
-    """Test number services."""
+async def test_select_service(hass, remove_platforms, bypass_initialize):
+    """Test select services."""
     # Create a mock entry so we don't have to go through config flow
     config_entry = await async_init_integration(hass)
     assert config_entry.state is ConfigEntryState.LOADED
@@ -47,15 +45,15 @@ async def test_number_service(hass, remove_platforms, bypass_initialize):
 
         attr = {
             ATTR_ENTITY_ID: entity_id,
-            ATTR_VALUE: expected_value,
+            ATTR_OPTION: expected_value,
         }
 
         await hass.services.async_call(
-            NUMBER_DOMAIN, SERVICE_SET_VALUE, attr, blocking=True
+            SELECT_DOMAIN, SERVICE_SELECT_OPTION, attr, blocking=True
         )
 
 
-async def test_number_exc(hass, remove_platforms, bypass_initialize, error_set_setting_exc):
+async def test_select_exc(hass, remove_platforms, bypass_initialize, error_set_setting_exc):
     """Test services with Skybell exception."""
     # Create a mock entry so we don't have to go through config flow
     config_entry = await async_init_integration(hass)
@@ -72,16 +70,16 @@ async def test_number_exc(hass, remove_platforms, bypass_initialize, error_set_s
 
     attr = {
         ATTR_ENTITY_ID: entity_id,
-        ATTR_VALUE: TEST_ENTITIES[0][2],
+        ATTR_OPTION: TEST_ENTITIES[0][2],
     }
 
     with pytest.raises(ServiceValidationError):
         assert await hass.services.async_call(
-            NUMBER_DOMAIN, SERVICE_SET_VALUE, attr, blocking=True
+            SELECT_DOMAIN, SERVICE_SELECT_OPTION, attr, blocking=True
         )
 
 
-async def test_number_acl(hass, remove_platforms, bypass_initialize, error_set_setting_acl):
+async def test_select_acl(hass, remove_platforms, bypass_initialize, error_set_setting_acl):
     """Test switch services with ACL exception."""
     # Create a mock entry so we don't have to go through config flow
     config_entry = await async_init_integration(hass)
@@ -98,10 +96,10 @@ async def test_number_acl(hass, remove_platforms, bypass_initialize, error_set_s
 
     attr = {
         ATTR_ENTITY_ID: entity_id,
-        ATTR_VALUE: TEST_ENTITIES[0][2],
+        ATTR_OPTION: TEST_ENTITIES[0][2],
     }
 
     with pytest.raises(ServiceValidationError):
         assert await hass.services.async_call(
-            NUMBER_DOMAIN, SERVICE_SET_VALUE, attr, blocking=True
+            SELECT_DOMAIN, SERVICE_SELECT_OPTION, attr, blocking=True
         )
